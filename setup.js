867c8a435221f48690667098e4ffcab266ca4cec
Tautvydas <etauker@gmail.com>
2018-08-16 21:52:24 +0100

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,6 +1,13 @@
-(function(exports) {
-    setup.install = function(oConfig) {
-        console.log("Starting config-atom setup - running setup.js");
-    }
-    module.exports = setup;
-})();
+var install = function(oConfig) {
+    console.log("Starting config-atom setup - running install");
+}
+var configure = function(oConfig) {
+    console.log("Starting config-atom setup - running configure");
+}
+var backup = function(oConfig) {
+    console.log("Starting config-atom setup - running backup");
+}
+
+module.exports.install = install;
+module.exports.configure = configure;
+module.exports.backup = backup;
