a5dc2df7245bf4baaeda803ad16ca7bb48497ff6
Tautvydas <t.kersulis1@nuigalway.ie>
2018-08-16 21:28:35 +0100

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,22 +1,6 @@
-let fs = require('fs');
-let helper = require('../development-setup/lib/helper.js');
-
-(function() {
-    console.log("Starting config-atom setup - running setup.js");
-    var aArgs = helper.getArguments();
-
-    // Set up each item under the settings option of configuration
-    aArgs["SETTINGS"].forEach(function(oSetting) {
-        var sConfigRepo = helper.extractRepoName(oSetting.repository);
-        var sBranch = oSetting.branch || "master";
-        if (!fs.existsSync(sConfigRepo)) {
-            helper.cloneRepository(oSetting.repository);
-        }
-        helper.changeDirectory(aArgs["PLATFORM"], aArgs["WORKSPACE"], sConfigRepo);
-        // helper.executeCommand("git checkout " + sBranch);
-        // helper.executeCommand("git pull origin " + sBranch);
-
-        // TODO: Carry out specific setup steps here...
-
-    });
+(function(exports) {
+    setup.install = function(oConfig) {
+        console.log("Starting config-atom setup - running setup.js");
+    }
+    module.exports = setup;
 })();
