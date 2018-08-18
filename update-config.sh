ee30b37c5ec0dfda3dad6a256549cc8aa0b51fc7
Tautvydas Kersulis <t.kersulis1@nuigalway.ie>
2018-08-18 22:23:04 +0100

    Added backupDir variable.

--- update-config.sh
+++ update-config.sh
@@ -26,27 +26,28 @@ if [[ $@ != *"-h"* && $@ != *"--help"* ]]; then
     GREEN="${RESET}\033[38;5;10m"
     YELLOW="${RESET}\033[38;5;11m"
     configDir="~/.atom"
+    backupDir="./content"
 
     # Install packages
     if [[ $@ = *"-i"* || $@ = *"--install"* ]]; then
         echo -e "${YELLOW}Installing Atom packages...${RESET}"
-        apm install --packages-file packages.list
+        eval "apm install --packages-file ${backupDir}/packages.list"
         echo -e "${GREEN}Atom package install complete.${RESET}"
     fi
 
     # Copy config files to ~/.atom
     echo -e "${YELLOW}Atom config import in progress...${RESET}"
     echo "-- Importing ${configDir}/config.cson"
-    eval "cp ./atom/config.cson ${configDir}/config.cson "
+    eval "cp ${backupDir}/atom/config.cson ${configDir}/config.cson "
     echo "-- Importing ${configDir}/helpers.js"
-    eval "cp ./atom/helpers.js ${configDir}/helpers.js "
+    eval "cp ${backupDir}/atom/helpers.js ${configDir}/helpers.js "
     echo "-- Importing ${configDir}/init.coffee"
-    eval "cp ./atom/init.coffee ${configDir}/init.coffee "
+    eval "cp ${backupDir}/atom/init.coffee ${configDir}/init.coffee "
     echo "-- Importing ${configDir}/keymap.cson"
-    eval "cp ./atom/keymap.cson ${configDir}/keymap.cson "
+    eval "cp ${backupDir}/atom/keymap.cson ${configDir}/keymap.cson "
     echo "-- Importing ${configDir}/snippets.cson"
-    eval "cp ./atom/snippets.cson ${configDir}/snippets.cson "
+    eval "cp ${backupDir}/atom/snippets.cson ${configDir}/snippets.cson "
     echo "-- Importing ${configDir}/styles.less"
-    eval "cp ./atom/styles.less ${configDir}/styles.less "
+    eval "cp ${backupDir}/atom/styles.less ${configDir}/styles.less "
     echo -e "${GREEN}Atom config import complete.${RESET}"
 fi
