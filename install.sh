6024cc53d1462f7019d56fa34cab035e7a277de9
Tautvydas Kersulis <t.kersulis1@nuigalway.ie>
2018-08-20 17:54:35 +0100

    Added install script.

---
+++ install.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Styling variables
+RESET="$(tput sgr0)"
+RED="${RESET}\033[38;5;1m"
+GREEN="${RESET}\033[38;5;10m"
+YELLOW="${RESET}\033[38;5;11m"
+GREY="${RESET}\e[37m"
+
+# Prints help page
+if [[ $@ = *"-h"* || $@ = *"--help"* ]]; then
+    echo -e "   ${GREY}"
+    echo -e "   This script installs atom and apm."
+    echo -e "   ${RESET}"
+
+    echo -e "   ${YELLOW}   -h, --help ${RESET}    Prints this message."
+
+    echo -e "   ${YELLOW}   --os ${RESET}   The operating system of the computer on which to install atom."
+    echo -e "               Accepted options: "
+    echo -e "                  --os=Windows [WIP] (default)"
+    echo -e "                  --os=Ubuntu [WIP]"
+    echo -e "                  --os=Debian [WIP]"
+    echo -e "                  --os=MacOS [WIP]"
+    echo -e "   ${RESET}"
+
+    # Windows (or no os option provided)
+elif [[ $@ = *"--os=Windows"* || $@ != *"--os="* ]]; then
+    echo -e "${RED}Atom installation on Windows is currently not supported.${RESET}"
+
+    # echo -e "${YELLOW}Atom download in progress...${RESET}"
+    # echo -e "${GREEN}Atom download complete.${RESET}"
+
+    # echo -e "${YELLOW}Atom installation in progress...${RESET}"
+    # echo -e "${GREEN}Atom installation complete.${RESET}"
+
+# Debian or Ubuntu
+elif [[ $@ = *"--os=Debian"* || $@ = *"--os=Ubuntu"* ]]; then
+    echo -e "${YELLOW}Atom download in progress...${RESET}"
+    # eval "curl -sL https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -"
+    # eval "sudo sh -c 'echo \"deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main\" > /etc/apt/sources.list.d/atom.list'"
+    # eval "sudo apt-get update"
+    echo -e "${GREEN}Atom download complete.${RESET}"
+
+    echo -e "${YELLOW}Atom installation in progress...${RESET}"
+    # eval "sudo apt-get install atom"
+    echo -e "${GREEN}Atom installation complete.${RESET}"
+
+# MacOS
+elif [[ $@ = *"--os=macOS"* ]]; then
+    echo -e "${RED}Atom installation on macOS is currently not supported.${RESET}"
+
+    # echo -e "${YELLOW}Atom download in progress...${RESET}"
+    # echo -e "${GREEN}Atom download complete.${RESET}"
+
+    # echo -e "${YELLOW}Atom installation in progress...${RESET}"
+    # echo -e "${GREEN}Atom installation complete.${RESET}"
+
+# Unsupported platform provided
+else
+    echo -e "${RED}Atom installation is currently not supported on the provided os.${RESET}"
+fi
--- setup.js
+++ setup.js
@@ -1,41 +1,38 @@
 const { execSync } = require('child_process');
 
+var _executeCommand = function(sCommand, bDebug) {
+    if (bDebug) console.log(`[debug] Executing command: ${sCommand}`);
+    try {
+        var stdout = execSync(sCommand);
+        console.log("Command Output: ");
+        console.log(stdout.toString('utf8'));
+    } catch (stderr) {
+        console.log("--------------------------------------------------");
+        console.log("Error executing command: " + sCommand);
+        console.log("--------------------------------------------------");
+        console.log("stderr: " + stderr);
+        console.log("--------------------------------------------------");
+    }
+};
+
 var install = function(oConfig) {
     console.log("Starting config-atom setup - running install");
+    process.chdir(oConfig.workspace + "/config-atom");
+    _executeCommand(`bash install.sh --os=${oConfig.platform}`, oConfig.options.debug);
 };
 
 var configure = function(oConfig) {
     console.log("Starting config-atom setup - running configure");
+    console.log("Changing directory to " + oConfig.workspace + "/config-atom");
     process.chdir(oConfig.workspace + "/config-atom");
-    _executeCommand("bash update-config.sh --install");
+    _executeCommand("bash update-config.sh --install", oConfig.options.debug);
 };
 
 var backup = function(oConfig) {
     console.log("Starting config-atom setup - running backup");
+    console.log("Changing directory to " + oConfig.workspace + "/config-atom");
     process.chdir(oConfig.workspace + "/config-atom");
-    _executeCommand("bash backup.sh --commit --backup");
-};
-
-function _executeCommand(sCommand) {
-    command = execSync(sCommand, function(err, stdout, stderr) {
-        if (err) {
-            // should have err.code here?
-        }
-
-        if (stderr) {
-            console.log("--------------------------------------------------");
-            console.log("Error executing command: " + sCommand);
-            console.log("--------------------------------------------------");
-            console.log("stderr: " + stderr);
-            console.log("--------------------------------------------------");
-        }
-        console.log("Command Output: ");
-        console.log(stdout);
-    });
-
-    // command.on('exit', function (code) {
-    //     console.log("exit code: " + code);
-    // });
+    _executeCommand("bash backup.sh --commit --push", oConfig.options.debug);
 };
 
 module.exports.install = install;
