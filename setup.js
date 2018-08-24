6eb171d114669a7a5ce007b0aec9e649636fa7e0
Tautvydas Kersulis <etauker@gmail.com>
2018-08-24 22:52:07 +0100

    Removed _executeCommand.

--- setup.js
+++ setup.js
@@ -1,21 +1,6 @@
 const { execSync } = require('child_process');
 let helper = require('../development-setup/lib/helper.js');
 
-var _executeCommand = function(sCommand, bDebug) {
-    if (bDebug) console.log(`[debug] Executing command: ${sCommand}`);
-    try {
-        var stdout = execSync(sCommand);
-        console.log("Command Output: ");
-        console.log(stdout.toString('utf8'));
-    } catch (stderr) {
-        console.log("--------------------------------------------------");
-        console.log("Error executing command: " + sCommand);
-        console.log("--------------------------------------------------");
-        console.log("stderr: " + stderr);
-        console.log("--------------------------------------------------");
-    }
-};
-
 var install = function(oConfig) {
     console.log("-- [config-atom] - running install");
     helper.changeDirectory(oConfig.platform, oConfig.workspace, "config-atom", oConfig.options.debug);
